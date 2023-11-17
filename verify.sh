7ac05cb632aa49956149f08c956d5c05362cf4bf
Appo <103305611+0xAppo@users.noreply.github.com>
2023-11-17 01:40:39 -0500

    deployment

---
+++ verify.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+npx hardhat verify-contract --network arbitrum --deployment-file-path "/Users/acanino/Desktop/code/lodestar/lodestar-public/lodestar-aggregator/src/deployments/arbitrum/ArbiDexAdapter.json"
